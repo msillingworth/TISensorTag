be336d27f5f1777a611d79219eefd6f3e56479d1
msillingworth <msillingworth@gmail.com>
2017-05-24 15:12:58 +0800

    Initial Commit

---
+++ TISensorTag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TISensorTag
+//
+//  Created by Mark Illingworth on 24/5/17.
+//  Copyright © 2017 Mark Illingworth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
