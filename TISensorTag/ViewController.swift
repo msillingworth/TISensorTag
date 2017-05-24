0c2ab36ccea9d4589209e6a206a5c6411ec46a67
msillingworth <msillingworth@gmail.com>
2017-05-24 15:41:30 +0800

    Branch 1_ Created three labels

--- TISensorTag/ViewController.swift
+++ TISensorTag/ViewController.swift
@@ -10,9 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - UI Labels
+    
+    var titleLabel: UILabel!
+    var statusLabel: UILabel!
+    var tempLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        // Set up each of these labels and add them as a subview to the main view
+    
+        // Set up title label
+        titleLabel = UILabel()
+        titleLabel.text = "My SensorTag"
+        titleLabel.font = UIFont(name: "HelveticaNeue-Bold", size: 20)
+        titleLabel.sizeToFit()
+        titleLabel.center = CGPoint(x: self.view.frame.midX, y: self.titleLabel.bounds.midY+28)
+        self.view.addSubview(titleLabel)
+        
+        // Set up status label
+        statusLabel = UILabel()
+        statusLabel.textAlignment = NSTextAlignment.center
+        statusLabel.text = "Loading..."
+        statusLabel.font = UIFont(name: "HelveticaNeue-Light", size: 12)
+        statusLabel.sizeToFit()
+        statusLabel.frame = CGRect(x: self.view.frame.origin.x, y: self.titleLabel.frame.maxY, width: self.view.frame.width, height: self.statusLabel.bounds.height)
+        self.view.addSubview(statusLabel)
+        
+        // Set up temperature label
+        tempLabel = UILabel()
+        tempLabel.text = "00.00"
+        tempLabel.font = UIFont(name: "HelveticaNeue-Bold", size: 72)
+        tempLabel.sizeToFit()
+        tempLabel.center = self.view.center
+        self.view.addSubview(tempLabel)
     }
 
     override func didReceiveMemoryWarning() {
